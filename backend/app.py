a554ce9a1331cd8b00e65d45e774be3059743a8f
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-03-07 00:04:30 +0200

    feature: initialize flask structure with main app runner, configurations and extensions

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from extensions import db, migrate
+from models.user import User
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)    
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
