781095a8f44c901255204d87013894f80ab9a83d
Kiguru-Alu <b.mahui@alustudent.com>
2026-03-07 14:59:04 +0200

    creating tables according to zone table design

--- backend/models/zone.py
+++ backend/models/zone.py
@@ -3,4 +3,27 @@ from ..extensions import db
 class Zone(db.Model):
     __tablename__ = "zones"
 
-    
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+   
+
+    created_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now()
+    )
+
+    updated_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        onupdate=db.func.now()
+    )
+
+    # indexing for fast location searching
+    __table_args__ = (
+        db.Index('idx_zone_location', 'latitude', 'longitude'),
+    )
+
+    def __repr__(self):
+        return f"<Zone {self.name}>"
