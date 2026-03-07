e7baf134875dcecd50e6b58614f4169c176dcf00
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-03-07 12:20:14 +0200

    fix: hashing length update

--- backend/models/user.py
+++ backend/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
     role = db.Column(
         db.Enum("resident", "zone_operator", "admin", name="user_role"),
         nullable=False,
