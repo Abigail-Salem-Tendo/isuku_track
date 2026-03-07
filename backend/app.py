1d8abbad7441bdec4c76175c5dd48e9b0e5245f2
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-03-07 18:43:45 +0200

    feature: authentication endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
-from extensions import db, migrate
+from extensions import db, migrate, bcrypt, jwt
 from models.user import User
+<<<<<<< HEAD
 from models.zone import Zone
 from models.vehicle import Vehicle
 from routes.vehicle_routes import vehicle_bp
+=======
+from routes.auth import auth_bp
+>>>>>>> df2cafc (feature: authentication endpoint)
 from config import Config
 
 def create_app():
@@ -11,7 +15,12 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app, db)    
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    
+    # Register blueprints
+    app.register_blueprint(auth_bp)
 
     return app
 
